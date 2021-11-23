583be8fb9f1e94d4b02d85a5085d837b2c74c326
dorigabay <dorigabay@gmail.com>
2021-11-23 13:10:47 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+
+def __main__ ():
+    seq_unarrenged = open("full_seq_arrenged.txt","r")
+    #seq_arrenged = open("full_seq_arrenged.txt","w+")
+    seq_unarrenged_string = seq_unarrenged.read()
+    count = 0
+    #for i in seq_unarrenged_string:
+     #   if i != " ":
+      #      # count+=1
+       #     seq_arrenged.write(i)
+        #    seq_arrenged.write("\n")
+    # print(count)
+    # seq_arrenged.close()
+    print(seq_unarrenged_string)
+
+if __name__ ==__main__():
+    main()
+
+import jupiter 
